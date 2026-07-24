138d33aad395c6aa9daa376762d6f02c9c3da841
emersud <emersud@gmail.com>
2026-07-24 12:20:32 -0300

    Initial commit: estrutura inicial do CRUD Spring

---
+++ src/main/java/com/example/crudprodutosjava/CrudProdutosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudprodutosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdutosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProdutosJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudprodutosjava/CrudProdutosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudprodutosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
