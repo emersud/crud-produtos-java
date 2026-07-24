a58030b22846444dbf5976c48bb3b8a08d3b3c83
ProfMagalhaes <emerson.dmagalhaes@docente.senai.br>
2026-07-24 15:30:31 -0300

    feat:implementada a funcionalidade de entidade projeto

--- src/main/java/com/example/crudprodutosjava/entities/Produtos.java
+++ src/main/java/com/example/crudprodutosjava/entities/Produtos.java
@@ -1,5 +1,49 @@
 package com.example.crudprodutosjava.entities;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "produtos")
 public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 120)
+	private String nome;
+
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
 }
