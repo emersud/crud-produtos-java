ef6929349e61d544b7345bb71e3a0d6f3bffe1bc
emersud <emersud@gmail.com>
2026-07-24 12:45:52 -0300

    adicionei as novas funções da camada de produtos

---
+++ src/main/java/com/example/crudprodutosjava/entities/Produtos.java
@@ -0,0 +1,5 @@
+package com.example.crudprodutosjava.entities;
+
+public class Produtos {
+
+}
---
+++ src/main/java/com/example/crudprodutosjava/repositoies/ProdutosRepository.java
@@ -0,0 +1,5 @@
+package com.example.crudprodutosjava.repositoies;
+
+public class ProdutosRepository {
+
+}
